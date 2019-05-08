1e888156dab5ccac1fadeaac26c1cbce5c8daddf
Nacho <iortega@atsistemas.com>
2019-05-08 20:45:02 +0200

    feat: HomePage route added and component imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent},
+  { path: '', component: HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
