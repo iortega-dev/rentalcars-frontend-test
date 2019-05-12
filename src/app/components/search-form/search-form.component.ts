77a1f2f3b4f025de21d02aac250b0dc3aa35c8bf
Nacho <iortega@atsistemas.com>
2019-05-12 15:57:25 +0200

    feat: search form implementation, call-to service and loading icon

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { SearchService } from 'src/app/services/search.service';
+import { Subject } from 'rxjs';
+import { takeUntil, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-form',
@@ -9,14 +12,44 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class SearchFormComponent implements OnInit {
   searchForm: FormGroup;
 
+  private destroySubject = new Subject();
+  showLoader = false;
+
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private searchSrv: SearchService
   ) {}
 
   ngOnInit() {
     this.searchForm = this.fb.group({
-      search: []
+      search: ['']
     });
+    this.initFormListener();
+  }
+
+  initFormListener() {
+    this.searchForm
+      .get('search')
+      .valueChanges.pipe(
+        takeUntil(this.destroySubject),
+        debounceTime(500),
+        filter(value => value.length >= 2)
+      )
+      .subscribe(val => {
+          this.doSearch(val);
+      });
+  }
+
+  doSearch(searchterm, numresults = 6) {
+    this.showLoader = true;
+    this.searchSrv.getSearchResults(numresults, searchterm).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => this.showLoader = false);
   }
 
 }
